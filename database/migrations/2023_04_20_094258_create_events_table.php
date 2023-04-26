c4032ce292b52f09d7c29355ae8304337f6ad1a5
Khizar Wahab <Khizarwahab19@gmail.com>
2023-04-26 18:35:24 +0500

    events, tickets, sales, categories

--- database/migrations/2023_04_20_094258_create_events_table.php
+++ database/migrations/2023_04_20_094258_create_events_table.php
@@ -15,15 +15,38 @@ return new class extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
-            $table->string("name");
+            $table->string("title");
             $table->text("description")->nullable()->default(null);
             $table->integer("event_category_id")->nullable()->default(null);
             $table->unsignedBigInteger("user_id")->nullable()->default(null);
             $table->dateTime("start_time")->nullable()->default(null);
             $table->dateTime("end_time")->nullable()->default(null);
-            $table->string("location")->nullable()->default(null);
+            $table->string("country")->nullable()->default(null);
+            $table->string("state")->nullable()->default(null);
+            $table->string("city")->nullable()->default(null);
+            $table->string("zipcode")->nullable()->default(null);
+            $table->string("address")->nullable()->default(null);
+            $table->string("image")->nullable()->default(null);
+            $table->string("sound")->nullable()->default(null);
             $table->tinyInteger("status")->default(0);
             $table->timestamps();
+
+            $table->string("promoter_first_name")->nullable()->default(null);
+            $table->string("promoter_last_name")->nullable()->default(null);
+            $table->string("promoter_email")->nullable()->default(null);
+            $table->string("promoter_phone_number")->nullable()->default(null);
+            $table->string("promoter_rojava_name")->nullable()->default(null);
+            $table->string("promoter_rojava_id")->nullable()->default(null);
+
+            $table->boolean("ticket_sale")->default(true);
+            $table->boolean("online_sale")->default(true);
+            $table->decimal("price", 10, 2)->nullable()->default(null);
+            $table->decimal("price_male", 10, 2)->nullable()->default(null);
+            $table->string("price_male_notification")->nullable()->default(null);
+            $table->decimal("price_female", 10, 2)->nullable()->default(null);
+            $table->string("price_female_notification")->nullable()->default(null);
+            $table->decimal("price_kids", 10, 2)->nullable()->default(null);
+            $table->string("price_kids_notification")->nullable()->default(null);
         });
     }
 
--- resources/views/content/events/edit.blade.php
+++ resources/views/content/events/edit.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts/layoutMaster')
 
-@section('title', 'Events - Create')
+@section('title', 'Events - Edit')
 
 @section('page-style')
 <link rel="stylesheet" href="{{asset('assets/vendor/css/pages/page-icons.css')}}" />
@@ -16,7 +16,7 @@
 @section('content')
 <h4 class="fw-bold py-3 mb-4 d-flex justify-content-between">
     <div>
-      <span class="text-muted fw-light">Events /</span> Add Event
+      <span class="text-muted fw-light">Events /</span> Edit Event
     </div>
     <div class="action-btns">
       <a href="{{ route('events.index') }}">
@@ -78,14 +78,15 @@
           </div>
         </div>
         <div class="bs-stepper-content">
-          <form action="{{ route('events.store') }}" method="post">
+          <form action="{{ route('events.update', $event->id) }}" method="post">
+            @method('PUT')
             @csrf
             <!-- About Event -->
             <div id="about-event" class="content">
               <div class="row g-3">
                 <div class="col-sm-12">
                   <label class="form-label" for="inputTitle">Title</label>
-                  <input type="text" id="inputTitle" name="title" value="{{ old('title') }}" class="form-control" placeholder="Event Title" />
+                  <input type="text" id="inputTitle" name="title" value="{{ old('title')?? $event->title }}" class="form-control" placeholder="Event Title" />
                   @error('title')
                     <div class="invalid-feedback d-block">{{ $message }}</div>
                   @enderror
@@ -93,7 +94,7 @@
                 <div class="col-sm-12">
                   <label class="form-label" for="inputDescription">Description</label>
                   <textarea id="inputDescription" name="description" class="form-control" placeholder="Event Description" rows="6">
-                    {{ old('description') }}
+                    {{ old('description')?? $event->description }}
                   </textarea>
                   @error('description')
                     <div class="invalid-feedback d-block">{{ $message }}</div>
@@ -104,7 +105,7 @@
                   <select id="inputCategory" name="event_category_id" class="form-control">
                     <option value="" selected disabled>Select</option>
                     @foreach($categories as $category)
-                    <option value="{{ $category->id }}" {{ (int) old('event_category_id') === $category->id? 'selected': ''  }}>{{ $category->name }}</option>
+                    <option value="{{ $category->id }}" {{ $event->event_category_id === $category->id? 'selected': ''  }}>{{ $category->name }}</option>
                     @endforeach
                   </select>
                   @error('event_category_id')
@@ -113,14 +114,14 @@
                 </div>
                 <div class="col-sm-6">
                   <label class="form-label" for="inputTitle">Image</label>
-                  <input type="file" id="inputImage" name="image" value="{{ old('image') }}" class="form-control" />
+                  <input type="file" id="inputImage" name="image" class="form-control" />
                   @error('image')
                     <div class="invalid-feedback d-block">{{ $message }}</div>
                   @enderror
                 </div>
                 <div class="col-sm-6">
                   <label class="form-label" for="inputTitle">Sound</label>
-                  <input type="file" id="inputImage" name="sound" value="{{ old('sound') }}" class="form-control" />
+                  <input type="file" id="inputImage" name="sound" class="form-control" />
                   @error('sound')
                     <div class="invalid-feedback d-block">{{ $message }}</div>
                   @enderror
@@ -142,21 +143,21 @@
               <div class="row g-3">
                 <div class="col-sm-12">
                   <label class="form-label" for="inputCountry">Country</label>
-                  <input type="text" id="inputCountry" name="country" value="{{ old('country') }}" class="form-control" placeholder="Event Country" />
+                  <input type="text" id="inputCountry" name="country" value="{{ old('country')?? $event->country }}" class="form-control" placeholder="Event Country" />
                   @error('country')
                     <div class="invalid-feedback d-block">{{ $message }}</div>
                   @enderror
                 </div>
                 <div class="col-sm-12">
                   <label class="form-label" for="inputZipcode">Zip Code</label>
-                  <input type="text" id="inputZipcode" name="zipcode" value="{{ old('zipcode') }}" class="form-control" placeholder="Event Zip Code" />
+                  <input type="text" id="inputZipcode" name="zipcode" value="{{ old('zipcode')?? $event->zipcode }}" class="form-control" placeholder="Event Zip Code" />
                   @error('zipcode')
                     <div class="invalid-feedback d-block">{{ $message }}</div>
                   @enderror
                 </div>
                 <div class="col-sm-12">
                   <label class="form-label" for="inputAddress">Address</label>
-                  <input type="text" id="inputAddress" name="address" value="{{ old('address') }}" class="form-control" placeholder="Event Address" />
+                  <input type="text" id="inputAddress" name="address" value="{{ old('address')?? $event->address }}" class="form-control" placeholder="Event Address" />
                   @error('address')
                     <div class="invalid-feedback d-block">{{ $message }}</div>
                   @enderror
@@ -178,42 +179,42 @@
               <div class="row g-3">
                 <div class="col-sm-6">
                   <label class="form-label" for="inputFirstName">First Name</label>
-                  <input type="text" id="inputFirstName" name="promoter_first_name" value="{{ old('promoter_first_name') }}" class="form-control" placeholder="Promoter First Name" />
+                  <input type="text" id="inputFirstName" name="promoter_first_name" value="{{ old('promoter_first_name')?? $event->promoter_first_name }}" class="form-control" placeholder="Promoter First Name" />
                   @error('promoter_first_name')
                     <div class="invalid-feedback d-block">{{ $message }}</div>
                   @enderror
                 </div>
                 <div class="col-sm-6">
                   <label class="form-label" for="inputLastName">Last Name</label>
-                  <input type="text" id="inputLastName" name="promoter_last_name" value="{{ old('promoter_last_name') }}" class="form-control" placeholder="Promoter Last Name" />
+                  <input type="text" id="inputLastName" name="promoter_last_name" value="{{ old('promoter_last_name')?? $event->promoter_last_name }}" class="form-control" placeholder="Promoter Last Name" />
                   @error('promoter_last_name')
                     <div class="invalid-feedback d-block">{{ $message }}</div>
                   @enderror
                 </div>
                 <div class="col-sm-6">
                   <label class="form-label" for="inputEmail">Email</label>
-                  <input type="text" id="inputEmail" name="promoter_email" value="{{ old('promoter_email') }}" class="form-control" placeholder="Promoter Email" />
+                  <input type="text" id="inputEmail" name="promoter_email" value="{{ old('promoter_email')?? $event->promoter_email }}" class="form-control" placeholder="Promoter Email" />
                   @error('promoter_email')
                     <div class="invalid-feedback d-block">{{ $message }}</div>
                   @enderror
                 </div>
                 <div class="col-sm-6">
                   <label class="form-label" for="inputPhoneNumber">Phone Number</label>
-                  <input type="text" id="inputPhoneNumber" name="promoter_phone_number" value="{{ old('promoter_phone_number') }}" class="form-control" placeholder="Promoter Phone Number" />
+                  <input type="text" id="inputPhoneNumber" name="promoter_phone_number" value="{{ old('promoter_phone_number')?? $event->promoter_phone_number }}" class="form-control" placeholder="Promoter Phone Number" />
                   @error('promoter_phone_number')
                     <div class="invalid-feedback d-block">{{ $message }}</div>
                   @enderror
                 </div>
                 <div class="col-sm-6">
                   <label class="form-label" for="inputRojavaName">Rojava Name</label>
-                  <input type="text" id="inputRojavaName" name="promoter_rojava_name" value="{{ old('promoter_rojava_name') }}" class="form-control" placeholder="Promoter Rojava Name" />
+                  <input type="text" id="inputRojavaName" name="promoter_rojava_name" value="{{ old('promoter_rojava_name')?? $event->promoter_rojava_name }}" class="form-control" placeholder="Promoter Rojava Name" />
                   @error('promoter_rojava_name')
                     <div class="invalid-feedback d-block">{{ $message }}</div>
                   @enderror
                 </div>
                 <div class="col-sm-6">
                   <label class="form-label" for="inputRojavaId">Rojava ID</label>
-                  <input type="text" id="inputRojavaId" name="promoter_rojava_id" value="{{ old('promoter_rojava_id') }}" class="form-control" placeholder="Promoter Rojava ID" />
+                  <input type="text" id="inputRojavaId" name="promoter_rojava_id" value="{{ old('promoter_rojava_id')?? $event->promoter_rojava_id }}" class="form-control" placeholder="Promoter Rojava ID" />
                   @error('promoter_rojava_id')
                     <div class="invalid-feedback d-block">{{ $message }}</div>
                   @enderror
@@ -237,8 +238,8 @@
                 <div class="col-sm-6">
                   <label class="form-label" for="inputTicketSale">Ticket Sale</label>
                   <select id="inputTicketSale" name="ticket_sale" class="form-control">
-                    <option value="true" {{ old('ticket_sale') === 'true'? 'selected': '' }}>Yes</option>
-                    <option value="false" {{ old('ticket_sale') === 'false'? 'selected': '' }}>No</option>
+                    <option value="true" {{ $event->ticket_sale? 'selected': '' }}>Yes</option>
+                    <option value="false" {{ !$event->ticket_sale? 'selected': '' }}>No</option>
                   </select>
                   @error('ticket_sale')
                     <div class="invalid-feedback d-block">{{ $message }}</div>
@@ -247,8 +248,8 @@
                 <div class="col-sm-6">
                   <label class="form-label" for="inputOnlineSale">Online Sale</label>
                   <select id="inputOnlineSale" name="online_sale" class="form-control">
-                    <option value="true" {{ old('online_sale') === 'true'? 'selected': '' }}>Yes</option>
-                    <option value="false" {{ old('online_sale') === 'false'? 'selected': '' }}>No</option>
+                    <option value="true" {{ $event->online_sale? 'selected': '' }}>Yes</option>
+                    <option value="false" {{ !$event->online_sale? 'selected': '' }}>No</option>
                   </select>
                   @error('ticket_sale')
                     <div class="invalid-feedback d-block">{{ $message }}</div>
@@ -256,42 +257,42 @@
                 </div>
                 <div class="col-sm-6">
                   <label class="form-label" for="inputPriceMale">Men Price</label>
-                  <input type="text" id="inputPriceMale" name="price_male" value="{{ old('price_male') }}" class="form-control" placeholder="Men Price" />
+                  <input type="text" id="inputPriceMale" name="price_male" value="{{ old('price_male')?? $event->price_male }}" class="form-control" placeholder="Men Price" />
                   @error('price_male')
                     <div class="invalid-feedback d-block">{{ $message }}</div>
                   @enderror
                 </div>
                 <div class="col-sm-6">
                   <label class="form-label" for="inputPriceMaleNotification" style="visibility:hidden;">Notification</label>
-                  <input type="text" id="inputPriceMaleNotification" name="price_male_notification" value="{{ old('price_male_notification') }}" class="form-control" placeholder="Notification" />
+                  <input type="text" id="inputPriceMaleNotification" name="price_male_notification" value="{{ old('price_male_notification')?? $event->price_male_notification }}" class="form-control" placeholder="Notification" />
                   @error('price_male_notification')
                     <div class="invalid-feedback d-block">{{ $message }}</div>
                   @enderror
                 </div>
                 <div class="col-sm-6">
                   <label class="form-label" for="inputPriceFemale">Women Price</label>
-                  <input type="text" id="inputPriceFemale" name="price_femail" value="{{ old('price_femail') }}" class="form-control" placeholder="Women Price" />
+                  <input type="text" id="inputPriceFemale" name="price_female" value="{{ old('price_female')?? $event->price_female }}" class="form-control" placeholder="Women Price" />
                   @error('price_femail')
                     <div class="invalid-feedback d-block">{{ $message }}</div>
                   @enderror
                 </div>
                 <div class="col-sm-6">
                   <label class="form-label" for="inputPriceFemailNotification" style="visibility:hidden;">Notification</label>
-                  <input type="text" id="inputPriceFemailNotification" name="price_female_notification" value="{{ old('price_female_notification') }}" class="form-control" placeholder="Notification" />
+                  <input type="text" id="inputPriceFemailNotification" name="price_female_notification" value="{{ old('price_female_notification')?? $event->price_female_notification }}" class="form-control" placeholder="Notification" />
                   @error('price_female_notification')
                     <div class="invalid-feedback d-block">{{ $message }}</div>
                   @enderror
                 </div>
                 <div class="col-sm-6">
                   <label class="form-label" for="inputPriceKids">Kids Price</label>
-                  <input type="text" id="inputPriceKids" name="price_kids" value="{{ old('price_kids') }}" class="form-control" placeholder="Kids Price" />
+                  <input type="text" id="inputPriceKids" name="price_kids" value="{{ old('price_kids')?? $event->price_kids }}" class="form-control" placeholder="Kids Price" />
                   @error('price_kids')
                     <div class="invalid-feedback d-block">{{ $message }}</div>
                   @enderror
                 </div>
                 <div class="col-sm-6">
                   <label class="form-label" for="inputPriceKidsNotification" style="visibility:hidden;">Notification</label>
-                  <input type="text" id="inputPriceKidsNotification" name="price_kids_notification" value="{{ old('price_kids_notification') }}" class="form-control" placeholder="Notification" />
+                  <input type="text" id="inputPriceKidsNotification" name="price_kids_notification" value="{{ old('price_kids_notification')?? $event->price_kids_notification }}" class="form-control" placeholder="Notification" />
                   @error('price_kids_notification')
                     <div class="invalid-feedback d-block">{{ $message }}</div>
                   @enderror
