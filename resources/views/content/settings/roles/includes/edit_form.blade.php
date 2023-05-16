9ac33c9ac44f1d10bbfded69dbe20e7c55c225fb
Khizar Wahab <Khizarwahab19@gmail.com>
2023-05-16 18:37:56 +0500

    Changes

--- resources/views/content/settings/roles/includes/edit_form.blade.php
+++ resources/views/content/settings/roles/includes/edit_form.blade.php
@@ -38,7 +38,7 @@
                     <td class="text-nowrap fw-semibold">{{ ucfirst(str_replace('_', ' ', $permission->name)) }}</td>
                     <td>
                       <div class="d-flex">
-                        @foreach ($permissions->where(fn($i) => $i->parent_id === $permission->id) as $childPermission)
+                        @foreach ($permissions->where(fn($i) => (int)$i->parent_id === (int)$permission->id) as $childPermission)
                         <div class="form-check me-3 me-lg-5">
                           <input class="form-check-input" type="checkbox" name="permissions[]" value="{{ $childPermission->name }}" id="permission{{ $childPermission->id }}-{{ $role->id }}" {{ $role->hasPermissionTo($childPermission->name)? 'checked': '' }} />
                           <label class="form-check-label" for="permission{{ $childPermission->id }}-{{ $role->id }}">
