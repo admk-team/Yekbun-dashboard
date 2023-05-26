bd563eaf34ce14253bda2e3bba60b7af339bfc48
ali9585 <alikhan9585497@gmail.com>
2023-05-26 11:09:54 +0500

    minor

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -117,7 +117,7 @@ class AuthController extends Controller
               'code' => $code,
             ];
             Mail::to($user->email)->send(new SendCodeMail($details));
-            return response()->json(['success' => true,'message' => 'A verification code has been sent to '.$user->email, 'data' => ['user_id'=>$user->id ,'email' => $user->email , 'token' => $token ]],201);
+            return response()->json(['success' => true,'message' => 'A verification email has been sent to '.$user->email.'!', 'data' => ['user_id'=>$user->id ,'email' => $user->email , 'token' => $token ]],201);
           } catch (Exception $e) {
             info('Error: ' . $e->getMessage());
           }
