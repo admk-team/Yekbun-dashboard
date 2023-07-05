41267a6ede75e9f36a29367023dba4dd46a0649a
ali9585 <alikhan9585497@gmail.com>
2023-07-05 13:07:22 +0500

    minor

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -22,6 +22,7 @@ class AuthController extends Controller
 {
   public function login(Request $request)
   {
+    return 'working';
     $credentials = $request->only('email', 'password');
 
     if (Auth::attempt($credentials)) {
