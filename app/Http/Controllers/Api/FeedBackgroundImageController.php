10e5cdccd0cd3e9b084e0665f654d456488ad268
ali9585 <alikhan9585497@gmail.com>
2023-07-10 15:25:27 +0500

    minor

--- app/Http/Controllers/Api/FeedBackgroundImageController.php
+++ app/Http/Controllers/Api/FeedBackgroundImageController.php
@@ -5,15 +5,22 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\BackgroundFeed;
-
+use App\Traits\UploadMedia;
 
 class FeedBackgroundImageController extends Controller
 {
+    use UploadMedia;
+
     public function upload(Request $request){
         
         $feed = new BackgroundFeed();
         $feed->title = $request->title;
-        $feed->image = $request->image;
+        
+        if ($request->hasFile('image'))
+            $path = UploadMedia::index($request->file('image'));
+
+        $feed->image = $path ?? '';
+
         if($feed->save()){
             return response()->json(['success' => true , 'data' =>$feed]);
         }else{
