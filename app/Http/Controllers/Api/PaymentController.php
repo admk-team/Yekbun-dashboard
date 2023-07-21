2814cee829a7bbe23159ff4719e289adafd9957f
ali9585 <alikhan9585497@gmail.com>
2023-07-21 12:51:39 +0500

    minor

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -10,16 +10,16 @@ use App\Models\Payment;
 class PaymentController extends Controller
 {
     private $gateway;
-    
+
     public function __construct()
     {
-        $redirect_link='';
+        $redirect_link = '';
         $this->gateway = Omnipay::create('PayPal_Rest');
         $this->gateway->setClientId(env('PAYPAL_CLIENT_ID'));
         $this->gateway->setSecret(env('PAYPAL_CLIENT_SECRET'));
         $this->gateway->setTestMode(true); //set it to 'false' when go live
     }
-  
+
     /**
      * Call a view.
      */
@@ -27,7 +27,7 @@ class PaymentController extends Controller
     {
         return view('payment');
     }
-  
+
     /**
      * Initiate a payment on PayPal.
      *
@@ -37,30 +37,33 @@ class PaymentController extends Controller
     {
         // if($request->input('submit'))
         // {
-            try {
-                $response = $this->gateway->purchase(array(
-                    'amount' => $request->input('amount') / 100,
-                    'currency' => env('PAYPAL_CURRENCY'),
-                    'returnUrl' => url('success'),
-                    'cancelUrl' => url('error'),
-                ))->send();
+        try {
+            $amountInDollars = $request->input('amount');
+            $amountInCents = $amountInDollars * 100; // Convert to cents
 
-                if ($response->isRedirect()) {
-                    $data = [
-                        'redirect_url' => $response->getRedirectUrl()
-                    ];
-                    return response()->json($data);
-                } else {
-                    // not successful
-                    return response()->json(['message' => $response->getMessage()], 500);
-                }
-            } catch(Exception $e) {
-                
-                return $e->getMessage();
+            $response = $this->gateway->purchase(array(
+                'amount' => $amountInCents,
+                'currency' => env('PAYPAL_CURRENCY'),
+                'returnUrl' => url('success'),
+                'cancelUrl' => url('error'),
+            ))->send();
+
+            if ($response->isRedirect()) {
+                $data = [
+                    'redirect_url' => $response->getRedirectUrl()
+                ];
+                return response()->json($data);
+            } else {
+                // not successful
+                return response()->json(['message' => $response->getMessage()], 500);
             }
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
         // }
     }
-  
+
+
     /**
      * Charge a payment and store the transaction.
      *
@@ -69,19 +72,17 @@ class PaymentController extends Controller
     public function success(Request $request)
     {
         // Once the transaction has been approved, we need to complete it.
-        if ($request->input('paymentId') && $request->input('PayerID'))
-        {
+        if ($request->input('paymentId') && $request->input('PayerID')) {
             $transaction = $this->gateway->completePurchase(array(
                 'payer_id'             => $request->input('PayerID'),
                 'transactionReference' => $request->input('paymentId'),
             ));
             $response = $transaction->send();
-          
-            if ($response->isSuccessful())
-            {
+
+            if ($response->isSuccessful()) {
                 // The customer has successfully paid.
                 $arr_body = $response->getData();
-          
+
                 // Insert transaction data into the database
                 $payment = new Payment;
                 $payment->payment_id = $arr_body['id'];
@@ -91,25 +92,25 @@ class PaymentController extends Controller
                 $payment->currency = env('PAYPAL_CURRENCY');
                 $payment->payment_status = $arr_body['state'];
                 $payment->save();
-        
+
                 return view('content.paypal.success');
                 // return "Payment is successful. Your transaction id is: ". $arr_body['id'];
             } else {
-                return response()->json(['success' => false , 'data' => $response->getMessage()]);
+                return response()->json(['success' => false, 'data' => $response->getMessage()]);
                 // return $response->getMessage();
             }
         } else {
-            return response()->json(['success' => false , 'data' => 'Transcation is declined.']);
+            return response()->json(['success' => false, 'data' => 'Transcation is declined.']);
             // return 'Transaction is declined';
         }
     }
-  
+
     /**
      * Error Handling.
      */
     public function error()
     {
-        return response()->json(['success' => false , 'data' => 'User Cancelled the payment.']);
+        return response()->json(['success' => false, 'data' => 'User Cancelled the payment.']);
         // return 'User cancelled the payment.';
     }
 }
