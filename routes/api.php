959fcae7e904ea7c6215bb3dc832c94c2ed975e1
ali9585 <alikhan9585497@gmail.com>
2023-08-04 12:16:12 +0500

    fetch all voting api

--- routes/api.php
+++ routes/api.php
@@ -226,7 +226,7 @@ Route::post('/history-search', [HistoryController::class, 'search']);
 // Voting
 Route::get('/voting-cover', [VotingController::class, 'get_cover']);
 Route::get('/fetch-voting', [VotingController::class, 'fetch']);
-Route::get('/voting-details', [VotingController::class, 'get_details']);
+Route::get('/voting-details/{id}', [VotingController::class, 'get_details']);
 Route::get('/fetch-voting/all', [VotingController::class, 'fetch_all']);
 Route::post('/voting/store-reaction', [VotingController::class, 'store_reaction']);
 
