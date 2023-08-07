95c018fc5c64c31c23cb49dc40567865cca3d3e2
ali9585 <alikhan9585497@gmail.com>
2023-08-07 17:10:48 +0500

    get comments api updated

--- app/Http/Controllers/Api/CommentController.php
+++ app/Http/Controllers/Api/CommentController.php
@@ -49,7 +49,7 @@ class CommentController extends Controller
         $comments = Comment::where($type, $id)->get();
     
         $formattedComments = $comments->map(function ($comment) {
-            $comment['formatted_created_at'] = $this->formatCreatedAt($comment->created_at);
+            $comment['created_at'] = $this->formatCreatedAt($comment->created_at);
             return $comment;
         });
     
