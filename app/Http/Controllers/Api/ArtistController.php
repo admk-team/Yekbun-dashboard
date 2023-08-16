cea9ea61d9375f87adef38aef91efcf4b7c93166
ali9585 <alikhan9585497@gmail.com>
2023-08-16 13:29:57 +0500

    minor

--- app/Http/Controllers/Api/ArtistController.php
+++ app/Http/Controllers/Api/ArtistController.php
@@ -23,10 +23,7 @@ class ArtistController extends Controller
 
     public function get_single_artist_music($id)
     {
-        $artist = Artist::select('id', 'image', 'first_name', 'last_name')
-            ->with('musics', 'province.country')
-            ->where('id', $id)
-            ->first();
+        $artist = Artist::select('id', 'image', 'first_name', 'last_name')->where('id', $id)->with('province.country')->first();
         if (is_null($artist)) {
             return response()->json(['success' => false, 'data' => []]);
         } else {
