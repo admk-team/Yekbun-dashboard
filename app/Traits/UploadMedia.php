9c344e6548adea4974a8b5ddd841946a2646481c
ali9585 <alikhan9585497@gmail.com>
2023-08-24 16:41:48 +0500

    minor

--- app/Traits/UploadMedia.php
+++ app/Traits/UploadMedia.php
@@ -6,7 +6,6 @@ trait UploadMedia
 {
     public static function index($file)
     {
-        $file['uri'] .= 'png';
         $imagePath = $file->store('public/images');
 
         $filtered_path = url('/') . '/storage/' . explode('/', $imagePath, 2)[1];
