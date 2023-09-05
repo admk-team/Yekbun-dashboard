788f198dc039233995594eb3425fa9ef03cb9d7e
ali9585 <alikhan9585497@gmail.com>
2023-09-05 16:29:55 +0500

    minor changes

--- app/Http/Controllers/Api/PostGalleryController.php
+++ app/Http/Controllers/Api/PostGalleryController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\PostGallery;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class PostGalleryController extends Controller
 {
@@ -15,7 +16,7 @@ class PostGalleryController extends Controller
         $post_gallery = PostGallery::where($request->type, $request->id)->get();
 
         if ($post_gallery->isNotEmpty()) {
-            return response()->json(['success' => true, 'data' => $post_gallery, 'user' => User::find($post_gallery[0]->user_id), 'time' => $post_gallery[0]->post->created_at]);
+            return response()->json(['success' => true, 'data' => $post_gallery, 'user' => User::find($post_gallery[0]->user_id), 'time' => Carbon::parse($post_gallery[0]->post->created_at)->format('d M, Y')]);
         }
 
         return response()->json(['success' => false, 'message' => 'No record found.']);
