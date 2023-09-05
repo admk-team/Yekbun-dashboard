d8e7a170a11c05404ec8d0fb90a641402169acb2
ali9585 <alikhan9585497@gmail.com>
2023-09-05 14:18:34 +0500

    minor

--- app/Http/Controllers/Api/FeedController.php
+++ app/Http/Controllers/Api/FeedController.php
@@ -173,7 +173,7 @@ class FeedController extends Controller
         });
 
         $data->map(function ($item) {
-            $item->media = $item->gallery->filter(function ($item_media) {
+            $item->gallery = $item->gallery->filter(function ($item_media) {
                 return $item_media->media_type != 1;
             });
         });
