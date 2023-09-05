0affc738b35cce80e814b36f087a37b337b874e0
ali9585 <alikhan9585497@gmail.com>
2023-09-05 15:38:43 +0500

    conflicts

--- app/Http/Controllers/Api/PostGalleryController.php
+++ app/Http/Controllers/Api/PostGalleryController.php
@@ -8,13 +8,17 @@ use Illuminate\Http\Request;
 
 class PostGalleryController extends Controller
 {
-    public function get_gallery(Request $request){
-     
-        $post_gallery = PostGallery::where($request->type,$request->id)->get();
-        if($post_gallery->isNotEmpty()){
-            return response()->json(['success' => true , 'data'=> $post_gallery]);
-        }
-        return response()->json(['success' => false , 'message'=> 'No record found.']);
+    public function get_gallery(Request $request)
+    {
 
+        $post_gallery = PostGallery::where($request->type, $request->id)->get();
+
+        $post_gallery['user'] = App\Models\User::find($post_gallery[0]->user_id);
+
+        if ($post_gallery->isNotEmpty()) {
+            return response()->json(['success' => true, 'data' => $post_gallery]);
+        }
+
+        return response()->json(['success' => false, 'message' => 'No record found.']);
     }
 }
