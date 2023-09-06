0c1784d8db645e34595ae52866230c6410a76df3
ali9585 <alikhan9585497@gmail.com>
2023-09-06 16:26:16 +0500

    api updated

--- app/Http/Controllers/Api/PostGalleryController.php
+++ app/Http/Controllers/Api/PostGalleryController.php
@@ -16,8 +16,13 @@ class PostGalleryController extends Controller
 
         $post_gallery = PostGallery::where($request->type, $request->id)->get();
 
+        $parent_table = explode('_', $request->type)[0];
+
+        if ($parent_table[strlen($parent_table) - 1] != 's')
+            $parent_table .= 's';
+
         if ($post_gallery->isNotEmpty()) {
-            return response()->json(['success' => true, 'data' => $post_gallery, 'user' => User::find($post_gallery[0]->user_id), 'time' => Carbon::parse(DB::table(explode('_', $request->type)[0])->find($request->id)->created_at)->format('M d Y')]);
+            return response()->json(['success' => true, 'data' => $post_gallery, 'user' => User::find($post_gallery[0]->user_id), 'time' => Carbon::parse(DB::table()->find($request->id)->created_at)->format('M d Y')]);
         }
 
         return response()->json(['success' => false, 'message' => 'No record found.']);
